ddea8f952f647f6a2349ed6f9a9f2d598ef6433e
Matrix Website <matrix@website.com>
2025-10-13 18:21:52 +0300

    Rebrand from Softec to Matrix: Updated all page titles and references

--- src/pages/home-2.jsx
+++ src/pages/home-2.jsx
@@ -6,7 +6,7 @@ import Wrapper from "../layout/wrapper";
 const index = () => {
   return (
     <Wrapper>
-      <SEO pageTitle={"Softec - Payment Gateway"} />
+      <SEO pageTitle={"Matrix - Payment Gateway"} />
       <HomeTwo />
     </Wrapper>
   );
